50c849f48108f52201beb89bb7f289e688311d46
jhude51 <jhudeyhemi51@gmail.com>
2026-04-10 21:31:47 +0100

    Added Serializer

--- app/issues/views.py
+++ app/issues/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Issue
+from .serializers import IssueSerializer
 
 class IssueList(APIView):
     def get(self, request):
-        issues = Issue.objects.all().values()
-        return Response(list(issues))
+        issues = Issue.objects.all().order_by('-created_at')
+        serializer = IssueSerializer(issues, many=True)
+        return Response(serializer.data)
